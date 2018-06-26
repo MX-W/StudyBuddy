9b84a7d2bb88b7b4d59175ffd1a981d51a80d82c
DWnk <david.wenkemann@fh-erfurt.de>
2018-06-26 13:31:44 +0200

    Fancier Menu
    Add icons, header and button - no actionbar in splashscreen

--- app/src/main/java/pme/ai/fhe/de/studybuddy/HomeActivity.java
+++ app/src/main/java/pme/ai/fhe/de/studybuddy/HomeActivity.java
@@ -1,8 +1,11 @@
 package pme.ai.fhe.de.studybuddy;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -12,10 +15,21 @@ import pme.ai.fhe.de.studybuddy.model.Daos.DataController;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         DataController controller = new DataController(getApplication());
         DataBase db = DataBase.getDatabase(getApplicationContext());
@@ -27,5 +41,13 @@ public class HomeActivity extends AppCompatActivity {
             String course = Integer.toString(cours.getCourseId());
             Log.i("CourseOfStudies-Dao: ", course);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/pme/ai/fhe/de/studybuddy/MainActivity.java
+++ app/src/main/java/pme/ai/fhe/de/studybuddy/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,13 +15,17 @@ import pme.ai.fhe.de.studybuddy.model.Daos.DataBase;
 import pme.ai.fhe.de.studybuddy.model.Daos.DataController;
 
 public class MainActivity extends AppCompatActivity {
-private static int SPLASH_TIME_OUT = 4000;
-private static String DATABASE_NAME = "StudyBuddy_DB";
+private static int SPLASH_TIME_OUT = 4000; //Time of the Splash Screen
+private static String DATABASE_NAME = "StudyBuddy_DB"; //Includes Database
 private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
         iv = (ImageView) findViewById(R.id.iv);
         Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
@@ -35,6 +40,7 @@ private ImageView iv;
         controller.insertCourse(computerScience2);
 
 
+        //Handler brings splash screen to the next activity
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
