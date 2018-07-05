5dc30661dde2ad25b513c9bffaece4abe6bf13b4
Max Wendl <wendl.max@gmail.com>
2018-07-05 15:23:19 +0200

    bugfixes

--- app/src/main/java/pme/ai/fhe/de/studybuddy/activities/Overview.java
+++ app/src/main/java/pme/ai/fhe/de/studybuddy/activities/Overview.java
@@ -1,12 +1,10 @@
 package pme.ai.fhe.de.studybuddy.activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ViewDebug;
 
 import com.facebook.stetho.Stetho;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -48,30 +46,32 @@ public class Overview extends MenuActivity {
         List<Lecture> pieDataset = controller.getLecturesByCourseId(dataset.getCourseId()); //gets List with All Courses from User
 
         int numberOfCategories = controller.getNumberOfCategories();
-        int [] categories = new int[numberOfCategories]; //Array Counts Categories
+        int [] categories = new int[numberOfCategories + 1]; //Array Counts Categories
 
 
-        Log.i("ID:", Integer.toString(dataset.getCourseId()));
+        //Log.i("ID:", Integer.toString(dataset.getCourseId()));
 
         for(Lecture l : pieDataset)
         {
             //todo if abfrage, ob kurs bestanden wurde
             //todo mit credit points multiplizieren
-            categories[l.getCategoryId()-1]++;
+            categories[l.getCategoryId()]++;
 
         }
 
 
-        List<PieEntry> piechartentry = new ArrayList<>(); //list of entrys
+        List<PieEntry> pieChartEntry = new ArrayList<>(); //list of entrys
         PieChart pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setEntryLabelColor(R.color.colorGrey);
+        pieChart.setEntryLabelTextSize(10.0f);
 
         //Add Data
-        for(int i = 0; i<numberOfCategories; i++)
+        for(int i = 1; i<=numberOfCategories; i++)
         {
             if(categories[i]>0)
             {
                 float value = (float) categories[i];
-                piechartentry.add(new PieEntry(value, controller.getCategorieNameByID(categories[i])+1));
+                pieChartEntry.add(new PieEntry(value, controller.getCategorieNameByID(i)));
             }
         }
 
@@ -83,21 +83,31 @@ public class Overview extends MenuActivity {
 
 
         // all the chart settings
-        PieDataSet pieentryset = new PieDataSet(piechartentry, "Übersicht");
-        PieData data2 = new PieData(pieentryset);
+        PieDataSet pieEntrySet = new PieDataSet(pieChartEntry, "Übersicht");
+        PieData data2 = new PieData(pieEntrySet);
         pieChart.setData(data2);
 
-        pieentryset.setColors(ColorTemplate.VORDIPLOM_COLORS); //color of chart
+        pieEntrySet.setColors(ColorTemplate.VORDIPLOM_COLORS); //color of chart
         pieChart.setUsePercentValues(true);
+
+        Description description = new Description();
+        description.setText("Deine bisherigen Leistungen nach Kategorien");
+        pieChart.setDescription(description);
+        //pieChart.setPadding(0,0,0,0);
         //pieChart.setCenterText("Dein Studium");
-        pieChart.setHoleRadius(30.0f);
-        pieChart.setTransparentCircleRadius(35.0f); //size of transparence inner circle
+        //pieChart.setHoleRadius(50.0f);
+        //pieChart.setTransparentCircleRadius(25.0f); //size of transparence inner circle
+        /*pieChart.setHoleColor(R.color.colorTransparentWhite);*/
+        pieChart.setDrawHoleEnabled(false);
         pieChart.setTransparentCircleAlpha(200); //transparence of inner circle
-        Legend l = pieChart.getLegend();
-        l.setFormSize(10f); // set the size of the legend forms/shapes
-        l.setForm(Legend.LegendForm.SQUARE); // set what type of form/shape should be used
-        l.setTextSize(14f);
-        l.setTextColor(Color.BLACK);
+        Legend legend = pieChart.getLegend();
+        legend.setFormSize(10f); // set the size of the legend forms/shapes
+        legend.setForm(Legend.LegendForm.SQUARE); // set what type of form/shape should be used
+        legend.setTextSize(14f);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setTextColor(R.color.colorGrey);
+        legend.setWordWrapEnabled(true);
+
         pieChart.animateXY(500, 500); // animate horizontal and vertical 500 milliseconds
         pieChart.invalidate(); // refresh
 
